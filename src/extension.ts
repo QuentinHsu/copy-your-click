c06f59363ad39f1e12160b7b41789b56916344c5
QuentinHsu <xuquentinyang@gmail.com>
2025-03-22 19:30:33 +0800

    feat(extension): file name and folder name copy function

--- src/extension.ts
+++ src/extension.ts
@@ -1,26 +1,24 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
 
-// This method is called when your extension is activated
-// Your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+    // 处理文件名复制
+    let copyFileNameCommand = vscode.commands.registerCommand('extension.copyFileName', async (uri: vscode.Uri) => {
+        if (uri) {
+            await vscode.env.clipboard.writeText(uri.fsPath.split('/').pop()!);
+            vscode.window.showInformationMessage(`Copied: ${uri.fsPath.split('/').pop()!}`);
+        }
+    });
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "copy-your-click" is now active!');
+    // 处理文件夹名复制
+    let copyFolderNameCommand = vscode.commands.registerCommand('extension.copyFolderName', async (uri: vscode.Uri) => {
+        if (uri) {
+            await vscode.env.clipboard.writeText(uri.fsPath.split('/').pop()!);
+            vscode.window.showInformationMessage(`Copied: ${uri.fsPath.split('/').pop()!}`);
+        }
+    });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('copy-your-click.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from copy your click!');
-	});
-
-	context.subscriptions.push(disposable);
+    context.subscriptions.push(copyFileNameCommand);
+    context.subscriptions.push(copyFolderNameCommand);
 }
 
-// This method is called when your extension is deactivated
 export function deactivate() {}
